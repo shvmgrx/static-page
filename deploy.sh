a62f54cbae7994e0920f88de5b81738f3cd56080
ngetahun <n.getahun@jacobs-university.de>
2017-07-25 20:41:50 +0200

    Updated main site shell script

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,7 @@ git pull
 git checkout master
 
 # Deploy main site
+git checkout master
 scss --update static-page/stylesheets/scss:static-page/stylesheets -f
-cp static-page/* /var/www/2016.jacobshack.com/ -Rf
+cp . /var/www/jacobshack.com/ -Rf
 
-# Update staging site
-git checkout gh-pages
-scss --update static-page/stylesheets/scss:static-page/stylesheets -f
-cp static-page/* /var/www/2016.jacobshack.com/dev/ -Rf
